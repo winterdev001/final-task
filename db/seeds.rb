38c78fc5c9bee5440eb472f0f57a0ed8660befbe
winterdev001 <snowdevin.sd@gmail.com>
2019-12-03 17:33:18 +0200

    reviewed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-# Company.create!(email:'alu@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'African Leadership University')
-# Company.create!(email:'kct@gmail.com',password:'kct123',location:'Town/Kigali',description:'Kigali City Tower')
-# Company.create!(email:'acgroup@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'alu')
-# Company.create!(email:'alu@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'alu')
-# Company.create!(email:'alu@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'alu')
-# Company.create!(email:'alu@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'alu')
-# Company.create!(email:'alu@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'alu')
-# Company.create!(email:'alu@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'alu')
-# Company.create!(email:'alu@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'alu')
-# Company.create!(email:'alu@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'alu')
-# Company.create!(email:'alu@gmail.com',password:'alu123',location:'Kacyiru/Kigali',description:'alu')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-# Worker.create!(email:'dev@gmail.com',password:'dev123',location:'Kacyiru/Kigali',skills:'web developer',hourly_rate:'25',description:'web developer')
-Administrator.create!(email:"admin2@gmail.com",password:"admin123",admin:true)
-
 1.upto(5) do |num|                                                   
     Company.create!(email:"alu#{num}@gmail.com",password:"alu123#{num}",username:"Alu#{num}",location:"Kacyiru#{num}/Kigali",description:"African Leadership University#{num}")       
 end   
@@ -41,8 +15,9 @@ end
 end  
 
 1.upto(12) do |num|                                                   
-    Job.create!(title: "Software Developer", deadline: "2019-12-1#{num} 1:40:00",  amount: "2#{num}", job_type:"2", location:"kigali#{num}",remote:"1",description:"web developer",company_id:"#{(1..5).rand}")       
+    Job.create!(title: "Software Developer", deadline: "2019-12-1#{num} 1:40:00",  amount: "2#{num}", job_type:"Fulltime", location:"kigali#{num}",remote:"1",description:"web developer",company_id:"#{(1..5).rand}")       
 end  
 
+Administrator.create!(email:"admin2@gmail.com",password:"admin123",admin:true)
 
    
