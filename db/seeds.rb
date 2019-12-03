6c1b8bfe2b53e708d127759d9152c860bef07167
winterdev001 <snowdevin.sd@gmail.com>
2019-12-03 18:50:21 +0200

    seeds updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 1.upto(5) do |num|                                                   
-#     Company.create!(email:"alu#{num}@gmail.com",password:"alu123#{num}",username:"Alu#{num}",location:"Kacyiru#{num}/Kigali",description:"African Leadership University#{num}")       
-# end   
+1.upto(5) do |num|                                                   
+    Company.create!(email:"alu#{num}@gmail.com",password:"alu123#{num}",username:"Alu#{num}",location:"Kacyiru#{num}/Kigali",description:"African Leadership University#{num}")       
+end   
 
-# 1.upto(5) do |num|                                                  
-#     Worker.create!(email:"dev#{num}@gmail.com",password:"dev123#{num}",username: "winter#{num}",location:"Kicukiro#{num}/Kigali",skills:"web developer",hourly_rate:"2#{num}",description:"web developer")       
-# end  
+1.upto(5) do |num|                                                  
+    Worker.create!(email:"dev#{num}@gmail.com",password:"dev123#{num}",username: "winter#{num}",location:"Kicukiro#{num}/Kigali",skills:"web developer",hourly_rate:"2#{num}",description:"web developer")       
+end  
 
-# 1.upto(12) do |num|                                                   
-#     Job.create!(title: "Software Developer", deadline: "2019-12-#{num} 1:40:00",  amount: "2#{num}", job_type:"Fulltime", location:"kigali#{num}",remote:"1",description:"web developer",company_id:1)       
-# end  
+1.upto(12) do |num|                                                   
+    Job.create!(title: "Software Developer", deadline: "2019-12-#{num} 1:40:00",  amount: "2#{num}", job_type:"Fulltime", location:"kigali#{num}",remote:"1",description:"web developer",company_id:1)       
+end  
 
 Administrator.create!(email:"admin2@gmail.com",password:"admin123",admin:true)
 
